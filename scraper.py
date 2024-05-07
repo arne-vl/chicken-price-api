7171da742a777c20907b7f99f1cce67466ed93a1
Arne Van Looveren <arne-vl@hotmail.be>
2024-05-07 17:17:32 +0200

    abc scraper

--- scraper.py
+++ scraper.py
@@ -2,13 +2,13 @@ import httpx
 from selectolax.parser import HTMLParser
 from datetime import datetime
 
-from util import write_to_csv
+from util import write_to_csv_deinze, write_to_csv_abc
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"
 }
 
-def save_price_deinze() -> float:
+def save_price_deinze():
     url = "https://www.pluimveeslachthuizen.be/nl/node/246"
 
     response = httpx.get(url=url, headers=HEADERS)
@@ -22,6 +22,24 @@ def save_price_deinze() -> float:
     notation_date = datetime.strptime(columns[0].text(), "%d/%m/%y").date()
     price_broilers = float(columns[4].text()[2:].replace(",", "."))
     
-    write_to_csv("data/deinze.csv", notation_date, price_broilers)
+    write_to_csv_deinze("data/deinze.csv", notation_date, price_broilers)
 
-save_price_deinze()
+def save_price_abc():
+    url = "https://www.pluimveeslachthuizen.be/nl/node/247"
+
+    response = httpx.get(url=url, headers=HEADERS)
+
+    html = HTMLParser(response.text)
+
+    most_recent_row = html.css("tr")[1]
+    
+    columns = most_recent_row.css("td")
+
+    notation_week = int(columns[0].text())
+    price_broilers = float(columns[1].text()[2:].replace(",", "."))
+
+    week_number = datetime.now().isocalendar()[1]
+    if(notation_week == week_number):
+        write_to_csv_abc("data/abc.csv", notation_week, price_broilers)
+
+save_price_abc()
--- util.py
+++ util.py
@@ -12,8 +12,19 @@ def date_exists_in_csv(filename: str, date: date):
         pass
     return False
 
-def write_to_csv(filename: str, date: date, value: float, max_length: int = 100):
-    if date_exists_in_csv(filename, date):
+def int_exists_in_csv(filename: str, number: int):
+    try:
+        with open(filename, 'r', newline='') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if row and row[0] == number:
+                    return True
+    except FileNotFoundError:
+        pass
+    return False
+
+def write_to_csv_deinze(filename: str, date: date, value: float, max_length: int = 100):
+    if int_exists_in_csv(filename, date):
         print("Date already exists in CSV file. Skipping...")
         return
     
@@ -27,6 +38,29 @@ def write_to_csv(filename: str, date: date, value: float, max_length: int = 100)
         pass
     
     existing_data.append([date.strftime('%Y-%m-%d'), value])
+    
+    if len(existing_data) > max_length:
+        existing_data = existing_data[-max_length:]
+    
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(existing_data)
+
+def write_to_csv_abc(filename: str, weeknr: int, value: float, max_length: int = 100):
+    if date_exists_in_csv(filename, weeknr):
+        print("Week number already exists in CSV file. Skipping...")
+        return
+    
+    existing_data = []
+    try:
+        with open(filename, 'r', newline='') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                existing_data.append(row)
+    except FileNotFoundError:
+        pass
+    
+    existing_data.append([weeknr, value])
     
     if len(existing_data) > max_length:
         existing_data = existing_data[-max_length:]
